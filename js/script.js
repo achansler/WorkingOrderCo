2f525e44e4ccc74c987d779a50dbc25914002103
achansler <aachansler@gmail.com>
2019-07-15 20:32:37 -0400

    fixed something that made the accordion clickable on load

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ $('#toggle').click(function() {
   });
 
 
+
 // back to top button
 
 var btn = $('#btt_button');
@@ -28,5 +29,25 @@ section.on('click', toggleAccordion);
 
 
 $( window ).scroll(function() {
-  $( "div.elixir_text_opener" ).addClass( "fade", 1000);
+  $( "div.elixir_text_opener" ).addClass( "fade" );
 });
+
+$( window ).scroll(function() {
+  $( ".awaken_section" ).delay(1000).addClass( "height_awaken");
+  $( ".unwind_section" ).delay(1000).addClass( "height_unwind");
+  setTimeout(function() { $( ".awaken_section" ).removeClass( "offscreen_awaken"); }, 1000);
+  setTimeout(function() { $( ".unwind_section" ).removeClass( "offscreen_unwind"); }, 1000);
+  setTimeout(function() { $( "footer" ).removeClass( "none"); }, 1000);
+  setTimeout(function() { $( ".need_sections" ).removeClass( "none"); }, 1000);
+  $( ".need_sections" ).delay(1000).addClass( "flex");
+  setTimeout(function() { $( "newsletter" ).removeClass( "none"); }, 1000);
+  setTimeout(function() { $( ".elixir_start" ).addClass( "none"); }, 1500);
+});
+
+$( window ).scroll(function() {
+  $(".white_rejuvenate").delay(1000).slideUp(500);
+});
+
+$('ul').click(function() {
+  $(this).removeClass('opener_height');
+})
