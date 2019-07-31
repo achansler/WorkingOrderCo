4fe46133c3ace3bf756181dafd37b32f2d7c77d7
achansler <aachansler@gmail.com>
2019-07-30 23:45:02 -0400

    added in some things so that stuff would be sticky in safari

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(function() {
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
 
-        if (scroll >= 400) {
+        if (scroll >= 310) {
             header.removeClass('order_logo_absolute').addClass("order_logo_stick");
         } else {
             header.removeClass("order_logo_stick").addClass('order_logo_absolute');
@@ -42,18 +42,22 @@ section.on('click', toggleAccordion);
 
 $( window ).one("scroll", function() {
   $( "div.elixir_text_opener" ).addClass( "fade" );
-  $(".white_rejuvenate").delay(500).slideUp(500);
+  // $(".white_rejuvenate").delay(500).slideUp(500);
+  $( ".white_rejuvenate" ).delay(500).animate({ height: "0%"
+}, 500, function() {
+    // Animation complete.
+  });
   $( ".awaken_section" ).delay(500).addClass( "height_awaken");
   $( ".unwind_section" ).delay(500).addClass( "height_unwind");
   setTimeout(function() { $( ".awaken_section" ).removeClass( "offscreen_awaken"); }, 500);
   setTimeout(function() { $( ".unwind_section" ).removeClass( "offscreen_unwind"); }, 500);
-  setTimeout(function() { $( "footer" ).removeClass( "none"); }, 1000);
-  setTimeout(function() { $( ".need_sections" ).removeClass( "none"); }, 1000);
-  $( ".need_sections" ).delay(1000).addClass( "flex");
-  setTimeout(function() { $( "newsletter" ).removeClass( "none"); }, 1000);
-  setTimeout(function() { $( ".elixir_start" ).addClass( "none"); }, 1500);
-  $( ".hide_title" ).delay(1500).addClass( "title");
-  setTimeout(function() { $( ".title" ).removeClass( "hide_title"); }, 1500);
+  setTimeout(function() { $( "footer" ).removeClass( "none"); }, 1500);
+  setTimeout(function() { $( ".need_sections" ).removeClass( "none"); }, 500);
+  $( ".need_sections" ).delay(500).addClass( "flex");
+  setTimeout(function() { $( "newsletter" ).removeClass( "none"); }, 1500);
+  setTimeout(function() { $( ".elixir_start" ).addClass( "none"); }, 1000);
+  $( ".hide_title" ).delay(1000).addClass( "title");
+  setTimeout(function() { $( ".title" ).removeClass( "hide_title"); }, 1000);
 });
 
 
